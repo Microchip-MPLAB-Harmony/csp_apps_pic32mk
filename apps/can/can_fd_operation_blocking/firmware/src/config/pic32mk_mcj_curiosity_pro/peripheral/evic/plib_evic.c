3434faef87a23abf728b615de6d4f260475d9ba6
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-08-19 19:52:13 +0530

    [CAN] Regenerated demo for can_can_fd_operation_blocking_pic32mk_mcj_curiosity_pro
    MH3-38899
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/can/can_fd_operation_blocking/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/evic/plib_evic.c
+++ apps/can/can_fd_operation_blocking/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/evic/plib_evic.c
@@ -43,6 +43,7 @@
 #include "device.h"
 #include "plib_evic.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: IRQ Implementation
@@ -53,7 +54,12 @@ void EVIC_Initialize( void )
 {
     INTCONSET = _INTCON_MVEC_MASK;
 
-    /* Set up priority / subpriority of enabled interrupts */
+    /* Set up priority and subpriority of enabled interrupts */
+
+
+
+    /* Configure Shadow Register Set */
+    PRISS = 0x76543210;
 }
 
 void EVIC_SourceEnable( INT_SOURCE source )
@@ -102,3 +108,5 @@ void EVIC_SourceStatusClear( INT_SOURCE source )
     *IFSxCLR = 1 << (source & 0x1f);
 }
 
+
+/* End of file */
