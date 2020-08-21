73abac77bc5bd4f9fb2d61a63e1e5a7a9e0295c2
Prachi Phadke <prachi.phadke@microchip.com>
2020-08-21 09:19:36 +0530

    [UART] Regenerated demo for uart_uart_ring_buffer_interrupt_pic32mk_mcm_curiosity_pro
    MH3-39027
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/uart/uart_ring_buffer_interrupt/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
+++ apps/uart/uart_ring_buffer_interrupt/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
@@ -65,17 +65,17 @@ void UART6_TX_InterruptHandler( void );
 
 
 /* All the handlers are defined here.  Each will call its PLIB-specific function. */
-void __ISR(_UART6_FAULT_VECTOR, ipl1AUTO) UART6_FAULT_Handler (void)
+void __ISR(_UART6_FAULT_VECTOR, ipl1SRS) UART6_FAULT_Handler (void)
 {
     UART6_FAULT_InterruptHandler();
 }
 
-void __ISR(_UART6_RX_VECTOR, ipl1AUTO) UART6_RX_Handler (void)
+void __ISR(_UART6_RX_VECTOR, ipl1SRS) UART6_RX_Handler (void)
 {
     UART6_RX_InterruptHandler();
 }
 
-void __ISR(_UART6_TX_VECTOR, ipl1AUTO) UART6_TX_Handler (void)
+void __ISR(_UART6_TX_VECTOR, ipl1SRS) UART6_TX_Handler (void)
 {
     UART6_TX_InterruptHandler();
 }
--- apps/uart/uart_ring_buffer_interrupt/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/uart/plib_uart6.c
+++ apps/uart/uart_ring_buffer_interrupt/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/uart/plib_uart6.c
@@ -105,7 +105,7 @@ void UART6_Initialize( void )
     /* Set up UxMODE bits */
     /* STSEL  = 0*/
     /* PDSEL = 0 */
-    /* BRGH = 0 */
+    /* BRGH = 1 */
     /* RXINV = 0 */
     /* ABAUD = 0 */
     /* LPBACK = 0 */
@@ -114,13 +114,13 @@ void UART6_Initialize( void )
     /* RUNOVF = 0 */
     /* CLKSEL = 0 */
     /* SLPEN = 0 */
-    U6MODE = 0x0;
+    U6MODE = 0x8;
 
     /* Enable UART6 Receiver, Transmitter and TX Interrupt selection */
-    U6STASET = (_U6STA_UTXEN_MASK | _U6STA_URXEN_MASK | _U6STA_UTXISEL0_MASK);
+    U6STASET = (_U6STA_UTXEN_MASK | _U6STA_URXEN_MASK | _U6STA_UTXISEL1_MASK);
 
     /* BAUD Rate register Setup */
-    U6BRG = 32;
+    U6BRG = 129;
 
     IEC5CLR = _IEC5_U6TXIE_MASK;
 
@@ -152,14 +152,21 @@ void UART6_Initialize( void )
 bool UART6_SerialSetup( UART_SERIAL_SETUP *setup, uint32_t srcClkFreq )
 {
     bool status = false;
-    uint32_t baud = setup->baudRate;
-    uint32_t brgValHigh = 0;
-    uint32_t brgValLow = 0;
+    uint32_t baud;
+    int32_t brgValHigh = 0;
+    int32_t brgValLow = 0;
     uint32_t brgVal = 0;
     uint32_t uartMode;
 
     if (setup != NULL)
     {
+        baud = setup->baudRate;
+
+        if (baud == 0)
+        {
+            return status;
+        }
+
         /* Turn OFF UART6 */
         U6MODECLR = _U6MODE_ON_MASK;
 
@@ -169,19 +176,19 @@ bool UART6_SerialSetup( UART_SERIAL_SETUP *setup, uint32_t srcClkFreq )
         }
 
         /* Calculate BRG value */
-        brgValLow = ((srcClkFreq / baud) >> 4) - 1;
-        brgValHigh = ((srcClkFreq / baud) >> 2) - 1;
+        brgValLow = (((srcClkFreq >> 4) + (baud >> 1)) / baud ) - 1;
+        brgValHigh = (((srcClkFreq >> 2) + (baud >> 1)) / baud ) - 1;
 
         /* Check if the baud value can be set with low baud settings */
-        if((brgValHigh >= 0) && (brgValHigh <= UINT16_MAX))
+        if((brgValLow >= 0) && (brgValLow <= UINT16_MAX))
         {
-            brgVal =  (((srcClkFreq >> 2) + (baud >> 1)) / baud ) - 1;
-            U6MODESET = _U6MODE_BRGH_MASK;
-        }
-        else if ((brgValLow >= 0) && (brgValLow <= UINT16_MAX))
-        {
-            brgVal = ( ((srcClkFreq >> 4) + (baud >> 1)) / baud ) - 1;
+            brgVal =  brgValLow;
             U6MODECLR = _U6MODE_BRGH_MASK;
+        }
+        else if ((brgValHigh >= 0) && (brgValHigh <= UINT16_MAX))
+        {
+            brgVal = brgValHigh;
+            U6MODESET = _U6MODE_BRGH_MASK;
         }
         else
         {
@@ -610,9 +617,6 @@ void UART6_FAULT_InterruptHandler (void)
 
 void UART6_RX_InterruptHandler (void)
 {
-    /* Clear UART6 RX Interrupt flag */
-    IFS5CLR = _IFS5_U6RXIF_MASK;
-
     /* Keep reading until there is a character availabe in the RX FIFO */
     while((U6STA & _U6STA_URXDA_MASK) == _U6STA_URXDA_MASK)
     {
@@ -625,6 +629,9 @@ void UART6_RX_InterruptHandler (void)
             /* UART RX buffer is full */
         }
     }
+
+    /* Clear UART6 RX Interrupt flag */
+    IFS5CLR = _IFS5_U6RXIF_MASK;
 }
 
 void UART6_TX_InterruptHandler (void)
@@ -634,9 +641,6 @@ void UART6_TX_InterruptHandler (void)
     /* Check if any data is pending for transmission */
     if (UART6_WritePendingBytesGet() > 0)
     {
-        /* Clear UART6TX Interrupt flag */
-        IFS5CLR = _IFS5_U6TXIF_MASK;
-
         /* Keep writing to the TX FIFO as long as there is space */
         while(!(U6STA & _U6STA_UTXBF_MASK))
         {
@@ -654,7 +658,11 @@ void UART6_TX_InterruptHandler (void)
                 break;
             }
         }
-	}
+
+        /* Clear UART6TX Interrupt flag */
+        IFS5CLR = _IFS5_U6TXIF_MASK;
+
+    }
     else
     {
         /* Nothing to transmit. Disable the data register empty interrupt. */
