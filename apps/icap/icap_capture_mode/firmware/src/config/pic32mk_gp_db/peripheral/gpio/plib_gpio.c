8d732d16d00b66ae51384830726b3f7ba9b126d4
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-10 23:21:50 +0530

    [ICAP] Regenerated demo for icap_icap_capture_mode_pic32mk_gp_db
    MH3-57180
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/icap/icap_capture_mode/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
+++ apps/icap/icap_capture_mode/firmware/src/config/pic32mk_gp_db/peripheral/gpio/plib_gpio.c
@@ -57,27 +57,22 @@
 */
 void GPIO_Initialize ( void )
 {
+
     /* PORTA Initialization */
     ANSELACLR = 0x8010; /* Digital Mode Enable */
-
     /* PORTB Initialization */
     ANSELBCLR = 0x200; /* Digital Mode Enable */
-
     /* PORTC Initialization */
-
     /* PORTD Initialization */
-
     /* PORTE Initialization */
-
     /* PORTF Initialization */
-
     /* PORTG Initialization */
-
 
     /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
+
     CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
@@ -90,8 +85,9 @@ void GPIO_Initialize ( void )
     RPA4R = 11;
     RPB9R = 5;
 
-    /* Lock back the system after PPS configuration */
+        /* Lock back the system after PPS configuration */
     CFGCONbits.IOLOCK = 1;
+
     SYSKEY = 0x00000000;
 
 }
