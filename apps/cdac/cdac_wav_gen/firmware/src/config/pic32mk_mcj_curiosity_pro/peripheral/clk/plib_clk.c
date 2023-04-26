fbb31de510136fff3d84a9bf5071956eeb8d727c
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-27 01:20:50 +0530

    [CDAC] Regenerated demo for cdac_cdac_wav_gen_pic32mk_mcj_curiosity_pro
    MH3-77738

--- apps/cdac/cdac_wav_gen/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
+++ apps/cdac/cdac_wav_gen/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/clk/plib_clk.c
@@ -85,9 +85,9 @@
 void CLK_Initialize( void )
 {
     /* unlock system for clock configuration */
-    SYSKEY = 0x00000000;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;
+    SYSKEY = 0x00000000U;
+    SYSKEY = 0xAA996655U;
+    SYSKEY = 0x556699AAU;
 
   
 
@@ -95,16 +95,16 @@ void CLK_Initialize( void )
 
     CFGCONbits.PMDLOCK = 0;
 
-    PMD1 = 0x341;
-    PMD2 = 0x17001f;
-    PMD3 = 0xffffffff;
-    PMD4 = 0xfff01fe;
-    PMD5 = 0xf3003f3f;
-    PMD6 = 0xf0d0000;
-    PMD7 = 0x0;
+    PMD1 = 0x341U;
+    PMD2 = 0x17001fU;
+    PMD3 = 0xffffffffU;
+    PMD4 = 0xfff01feU;
+    PMD5 = 0xf3003f3fU;
+    PMD6 = 0xf0d0000U;
+    PMD7 = 0x0U;
 
     CFGCONbits.PMDLOCK = 1;
 
     /* Lock system since done with clock configuration */
-    SYSKEY = 0x33333333;
+    SYSKEY = 0x33333333U;
 }
