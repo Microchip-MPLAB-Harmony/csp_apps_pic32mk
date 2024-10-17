13f07be4c15892a138a2ad364a3c53545828f924
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:14:37 +0530

    [TMR] Regenerated demo for tmr__tmr_timer_mode__firmware__pic32mk_mcj_curiosity_pro.X
    MH3-100616

--- apps/tmr/tmr_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/gpio/plib_gpio.h
+++ apps/tmr/tmr_timer_mode/firmware/src/config/pic32mk_mcj_curiosity_pro/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define LED2_OutputEnable()      (TRISACLR = (1U<<10))
 #define LED2_InputEnable()       (TRISASET = (1U<<10))
 #define LED2_Get()               ((PORTA >> 10) & 0x1U)
+#define LED2_GetLatch()          ((LATA >> 10) & 0x1U)
 #define LED2_PIN                  GPIO_PIN_RA10
 
 
@@ -218,7 +219,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
