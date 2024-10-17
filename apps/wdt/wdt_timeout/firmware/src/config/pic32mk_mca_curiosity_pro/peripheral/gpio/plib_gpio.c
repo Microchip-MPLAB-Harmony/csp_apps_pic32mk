94079324cf085dff432c56edd8ab72c223ffeb58
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:07:41 +0530

    [WDT] Regenerated demo for wdt__wdt_timeout__firmware__pic32mk_mca_curiosity_pro.X
    MH3-100652

--- apps/wdt/wdt_timeout/firmware/src/config/pic32mk_mca_curiosity_pro/peripheral/gpio/plib_gpio.c
+++ apps/wdt/wdt_timeout/firmware/src/config/pic32mk_mca_curiosity_pro/peripheral/gpio/plib_gpio.c
@@ -43,7 +43,6 @@
 
 #include "plib_gpio.h"
 #include "interrupts.h"
-#include "interrupts.h"
 
 
 
@@ -66,7 +65,6 @@ void GPIO_Initialize ( void )
     TRISACLR = 0x400U; /* Direction Control */
     /* PORTB Initialization */
     /* PORTC Initialization */
-    ANSELCCLR = 0x400U; /* Digital Mode Enable */
     /* PORTD Initialization */
 
     /* Unlock system for PPS configuration */
@@ -77,10 +75,10 @@ void GPIO_Initialize ( void )
     CFGCONbits.IOLOCK = 0U;
 
     /* PPS Input Remapping */
-    U2RXR = 4;
+    U2RXR = 6;
 
     /* PPS Output Remapping */
-    RPC10R = 2;
+    RPD8R = 2;
 
         /* Lock back the system after PPS configuration */
     CFGCONbits.IOLOCK = 1U;
