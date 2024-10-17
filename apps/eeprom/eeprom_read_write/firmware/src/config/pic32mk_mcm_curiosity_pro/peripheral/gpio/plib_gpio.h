15be07b813e04a0fd4c666976b601be64febb379
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:21:48 +0530

    [EEPROM] Regenerated demo for eeprom__eeprom_read_write__firmware__pic32mk_mcm_curiosity_pro.X
    MH3-100526

--- apps/eeprom/eeprom_read_write/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/gpio/plib_gpio.h
+++ apps/eeprom/eeprom_read_write/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define LED1_OutputEnable()      (TRISGCLR = (1U<<12))
 #define LED1_InputEnable()       (TRISGSET = (1U<<12))
 #define LED1_Get()               ((PORTG >> 12) & 0x1U)
+#define LED1_GetLatch()          ((LATG >> 12) & 0x1U)
 #define LED1_PIN                  GPIO_PIN_RG12
 
 
@@ -243,7 +244,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
