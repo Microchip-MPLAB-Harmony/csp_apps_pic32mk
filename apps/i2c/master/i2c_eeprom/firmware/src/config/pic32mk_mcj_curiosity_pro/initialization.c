b53081d648d495294f2590deb2c1b857468e7642
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:14:27 +0530

    [I2C] Regenerated demo for i2c__master__i2c_eeprom__firmware__pic32mk_mcj_curiosity_pro.X
    MH3-100544

--- apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mk_mcj_curiosity_pro/initialization.c
+++ apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mk_mcj_curiosity_pro/initialization.c
@@ -47,7 +47,6 @@
 #include "device.h"
 
 
-
 // ****************************************************************************
 // ****************************************************************************
 // Section: Configuration Bits
@@ -113,6 +112,12 @@
 // Section: Driver Initialization Data
 // *****************************************************************************
 // *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 - Deviation record ID - H3_MISRAC_2012_R_7_2_DR_1 */
+/* MISRA C-2012 Rule 11.1 - Deviation record ID - H3_MISRAC_2012_R_11_1_DR_1 */
+/* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+/* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
 
 
 // *****************************************************************************
@@ -142,7 +147,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
+/* MISRAC 2012 deviation block end */
 
 /*******************************************************************************
   Function:
@@ -156,6 +161,7 @@
 
 void SYS_Initialize ( void* data )
 {
+
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
 
@@ -179,18 +185,15 @@ void SYS_Initialize ( void* data )
     I2C2_Initialize();
 
 
-
-
-
     EVIC_Initialize();
 
 	/* Enable global interrupts */
     (void)__builtin_enable_interrupts();
 
 
+
     /* MISRAC 2012 deviation block end */
 }
-
 
 /*******************************************************************************
  End of File
