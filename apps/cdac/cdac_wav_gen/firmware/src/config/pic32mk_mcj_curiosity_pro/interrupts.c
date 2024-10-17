65c1cf95a1e6509c6dad127880ab3911f32a8c5b
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:14:22 +0530

    [CDAC] Regenerated demo for cdac__cdac_wav_gen__firmware__pic32mk_mcj_curiosity_pro.X
    MH3-100494

--- apps/cdac/cdac_wav_gen/firmware/src/config/pic32mk_mcj_curiosity_pro/interrupts.c
+++ apps/cdac/cdac_wav_gen/firmware/src/config/pic32mk_mcj_curiosity_pro/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -74,7 +74,7 @@ void TIMER_1_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_TIMER_1_VECTOR, ipl1SRS) TIMER_1_Handler (void)
+void __attribute__((used)) __ISR(_TIMER_1_VECTOR, ipl1SRS) TIMER_1_Handler (void)
 {
     TIMER_1_InterruptHandler();
 }
