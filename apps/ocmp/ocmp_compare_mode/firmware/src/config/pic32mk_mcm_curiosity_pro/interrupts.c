f941f4bd6a11d8c574f870ff05a4b226478367b1
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:21:53 +0530

    [OCMP] Regenerated demo for ocmp__ocmp_compare_mode__firmware__pic32mk_mcm_curiosity_pro.X
    MH3-100578

--- apps/ocmp/ocmp_compare_mode/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
+++ apps/ocmp/ocmp_compare_mode/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -74,7 +74,7 @@ void TIMER_2_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_TIMER_2_VECTOR, ipl1SRS) TIMER_2_Handler (void)
+void __attribute__((used)) __ISR(_TIMER_2_VECTOR, ipl1SRS) TIMER_2_Handler (void)
 {
     TIMER_2_InterruptHandler();
 }
