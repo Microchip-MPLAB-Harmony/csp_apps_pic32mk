b2910fc96901dd05a555195e5f992338a0974c2b
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:18:57 +0530

    [CORETIMER] Regenerated demo for coretimer__coretimer_periodic_timeout__firmware__pic32mk_mcm_curiosity_pro.X
    MH3-100514

--- apps/coretimer/coretimer_periodic_timeout/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
+++ apps/coretimer/coretimer_periodic_timeout/firmware/src/config/pic32mk_mcm_curiosity_pro/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -74,7 +74,7 @@ void CORE_TIMER_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_CORE_TIMER_VECTOR, ipl1SRS) CORE_TIMER_Handler (void)
+void __attribute__((used)) __ISR(_CORE_TIMER_VECTOR, ipl1SRS) CORE_TIMER_Handler (void)
 {
     CORE_TIMER_InterruptHandler();
 }
