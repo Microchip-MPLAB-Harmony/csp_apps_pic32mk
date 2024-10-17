4b6cbe4a61d344486d335ca573227f1f397534bd
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-10-17 16:03:35 +0530

    [ADCHS] Regenerated demo for adchs__adchs_interrupt__firmware__pic32mk_gp_db.X
    MH3-100466

--- apps/adchs/adchs_interrupt/firmware/src/config/pic32mk_gp_db/interrupts.c
+++ apps/adchs/adchs_interrupt/firmware/src/config/pic32mk_gp_db/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -74,7 +74,7 @@ void ADC_DATA3_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_ADC_DATA3_VECTOR, ipl1SOFT) ADC_DATA3_Handler (void)
+void __attribute__((used)) __ISR(_ADC_DATA3_VECTOR, ipl1SOFT) ADC_DATA3_Handler (void)
 {
     ADC_DATA3_InterruptHandler();
 }
